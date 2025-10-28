07d20ecf90038a8908843ea3b366d5c9c0528661
Varun Yadav <varunyadav191101@gmail.com>
2025-10-28 10:26:53 +0530

    Initial commit

---
+++ server/controllers/UserController.js
@@ -0,0 +1,6 @@
+// Api controller function to manage clerk user with database
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (req, res) => {
+    
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+// App Config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+// Initialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API Routes 
+app.get('/', (req, res) => res.send(`API Working`))
+
+app.listen(PORT, ()=> console.log("Server Running on port "+PORT))
